10cdd260e0a6bec81c8be0a261851661d5a30059
Cadence Ember <cadence@disroot.org>
2022-10-04 21:00:33 +1300

    Close response after error in proxy

--- src/page-proxy.rkt
+++ src/page-proxy.rkt
@@ -18,13 +18,16 @@
   (match (dict-ref (url-query (request-uri req)) 'dest #f)
     [(? string? dest)
      (if (is-fandom-url? dest)
-         (response-handler
+         (response-handler ; catches and reports errors
           (let ([dest-r (easy:get dest #:stream? #t)])
-            (response/output
-             #:code (easy:response-status-code dest-r)
-             #:mime-type (easy:response-headers-ref dest-r 'content-type)
-             (λ (out)
-               (copy-port (easy:response-output dest-r) out)
-               (easy:response-close! dest-r)))))
+            (with-handlers ([exn:fail? (λ (e) ; cleans up and re-throws
+                                         (easy:response-close! dest-r)
+                                         (raise e))])
+              (response/output
+               #:code (easy:response-status-code dest-r)
+               #:mime-type (easy:response-headers-ref dest-r 'content-type)
+               (λ (out)
+                 (copy-port (easy:response-output dest-r) out)
+                 (easy:response-close! dest-r))))))
          (next-dispatcher))]
     [#f (next-dispatcher)]))
