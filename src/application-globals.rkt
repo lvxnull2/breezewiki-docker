b8e5fb8dc536be683c201132665fd4431738d1ae
Cadence Ember <cadence@disroot.org>
2022-12-10 19:16:28 +1300

    nofollow to fandom.com (adding one that I missed)

--- src/application-globals.rkt
+++ src/application-globals.rkt
@@ -69,7 +69,7 @@
                      `(div (p "This page displays proxied content from "
                               (a (@ (href ,source-url) (rel "nofollow noreferrer")) ,source-url)
                               ,(format ". Text content is available under the ~a license, " (license^-text license))
-                              (a (@ (href ,(license^-url license))) "see license info.")
+                              (a (@ (href ,(license^-url license)) (rel "nofollow")) "see license info.")
                               " Media files may have different copying restrictions.")
                            (p ,(format "Fandom is a trademark of Fandom, Inc. ~a is not affiliated with Fandom." (config-get 'application_name))))
                      `(div (p "Text content on wikis run by Fandom is available under the Creative Commons Attribution-Share Alike License 3.0 (Unported), "
