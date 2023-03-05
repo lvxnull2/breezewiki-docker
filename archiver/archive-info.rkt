29007b0e29c5ecff6a7b6824086bb08c6758ac71
Cadence Ember <cadence@disroot.org>
2023-03-05 22:34:51 +1300

    Add req.rktd for use with raco req

--- archiver/archive-info.rkt
+++
@@ -1,3 +0,0 @@
-#lang info
-
-(define build-deps '("rackunit-lib" "web-server-lib" "http-easy-lib" "html-parsing" "html-writing" "json-pointer" "ini-lib" "memo" "net-cookies-lib" "gui-easy-lib" "sql" "charterm" "cli"))
